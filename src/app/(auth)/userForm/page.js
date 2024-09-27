9995ec4b6d9088df324e57bc6fefe18041db4617
MachineMuggle <ishaangupta1104@gmail.com>
2024-09-27 16:29:40 +0530

    add page.js for userForm route

---
+++ src/app/(auth)/userForm/page.js
@@ -0,0 +1,9 @@
+import UserOnboardingForm from "@/components/UserForm";
+
+export default function UserFormPage() {
+  return (
+    <main className="h-screen min-h-screen">
+      <UserOnboardingForm />
+    </main>
+  );
+}
