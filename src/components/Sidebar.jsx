b505fbc1c13ab3b41fd0dddbd3c3a03283e81cb0
MachineMuggle <ishaangupta1104@gmail.com>
2024-10-04 19:25:35 +0530

    Change Sidebar content

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import React from "react";
+import PropTypes from "prop-types";
+
 import {
   Sheet,
   SheetClose,
@@ -12,7 +13,26 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 
+import {
+  Bird,
+  Book,
+  Bot,
+  Code2,
+  CornerDownLeft,
+  LifeBuoy,
+  Mic,
+  Paperclip,
+  Rabbit,
+  Settings,
+  Settings2,
+  Share,
+  SquareTerminal,
+  SquareUser,
+  Turtle,
+} from "lucide-react";
+
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { ModeToggle } from "./ModeToggle";
 
 export function Sidebar() {
   return (
@@ -23,33 +43,52 @@ export function Sidebar() {
           <AvatarFallback className=" bg-gray-200">?</AvatarFallback>
         </Avatar>
       </SheetTrigger>
-      <SheetContent side={'left'}>
-        <SheetHeader>
-          <SheetTitle>Edit profile</SheetTitle>
-          <SheetDescription>
-            Make changes to your profile here. Click save when you&apos;re done.
-          </SheetDescription>
-        </SheetHeader>
-        <div className="dark grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
-          </div>
-        </div>
-        <SheetFooter>
-          <SheetClose asChild>
-            <Button type="submit">Save changes</Button>
-          </SheetClose>
-        </SheetFooter>
+
+      <SheetContent className="w-[300px] sm:w-[300px]" side={"left"}>
+        <ModeToggle />
+        <nav className="grid gap-1 p-2">
+          <SidebarItem
+            icon={<SquareTerminal className="size-5" />}
+            text="Playground"
+          />
+          <SidebarItem
+            icon={<Book className="size-5" />}
+            text="Documentation"
+          />
+          <SidebarItem
+            icon={<Settings2 className="size-5" />}
+            text="Settings"
+          />
+          <SidebarItem icon={<LifeBuoy className="size-5" />} text="Help" />
+          <SidebarItem
+            icon={<SquareUser className="size-5" />}
+            text="Account"
+          />
+        </nav>
       </SheetContent>
     </Sheet>
   );
 }
+
+export function SidebarItem({ icon, text, active }) {
+  return (
+    <Button
+      variant="ghost"
+      className={`w-full justify-start rounded-lg ${active ? "bg-accent" : ""}`}
+      aria-label={text}
+    >
+      {icon}
+      <span className="ml-2">{text}</span>
+    </Button>
+  );
+}
+
+SidebarItem.propTypes = {
+  icon: PropTypes.node,
+  text: PropTypes.string,
+  active: PropTypes.bool,
+};
+
+SidebarItem.defaultProps = {
+  active: false,
+};
