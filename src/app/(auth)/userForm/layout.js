a251ff92de5ba299f3eeee48a15ad14e6ed908de
MachineMuggle <ishaangupta1104@gmail.com>
2025-03-13 22:54:39 +0530

    Add clerk providers to UserFormLayout

--- src/app/(auth)/userForm/layout.js
+++ src/app/(auth)/userForm/layout.js
@@ -10,16 +10,18 @@ import {
 export default function UserFormLayout({ children }) {
   return (
     <section>
-      <div className="flex justify-center items-center h-screen">
-        <SignedOut>
-          <SignInButton />
-          <SignUpButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton className="border-2 border-black" />
-        </SignedIn>
-        {children}
-      </div>
+      <ClerkProvider>
+        <div className="flex justify-center items-center h-screen">
+          <SignedOut>
+            <SignInButton />
+            <SignUpButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton className="border-2 border-black" />
+          </SignedIn>
+          {children}
+        </div>
+      </ClerkProvider>
     </section>
   );
 }
