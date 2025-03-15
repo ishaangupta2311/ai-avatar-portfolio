c7ea30e1e69b4594235b4a6b6701a9bd0025cd2b
MachineMuggle <ishaangupta1104@gmail.com>
2025-03-15 19:43:21 +0530

    minor fixes

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,20 +13,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-
-      <html lang="en">
-        <body className={inter.className}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            
-            {children}
-          </ThemeProvider>
-        </body>
-      </html>
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
 
   );
 }
